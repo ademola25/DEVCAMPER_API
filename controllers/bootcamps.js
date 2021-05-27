909e0445d726a8ae7be77c936bbe45c8535ba927
tijani ademola <ademola.tijani@softescu.com>
2021-05-27 04:05:29 +0100

    erro herer handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 
 //@desc Get all bootcamps
@@ -13,9 +14,7 @@ exports.getBootcamps = async (req, res, next) => {
             data: bootcamps
         });
     } catch (err) {
-        res.status(400).json({
-            success: false
-        });
+        next(err);
     }
 }
 //@desc Get single bootcamps
@@ -26,18 +25,14 @@ exports.getBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id);
 
         if (!bootcamp) {
-            return res.status(400).json({
-                success: false
-            });
+            return next(new ErrorResponse(`Bootcamp not found  id of ${req.params.id}`, 404));
         }
         res.status(200).json({
             success: true,
             data: bootcamp
         });
     } catch (err) {
-        res.status(400).json({
-            success: false
-        });
+        next(err);
     }
 }
 //@desc create bootcamps
@@ -52,9 +47,7 @@ exports.createBootcamp = async (req, res, next) => {
             data: bootcamp
         });
     } catch (err) {
-        res.status(400).json({
-            success: false
-        });
+        next(err);
     }
 
 };
@@ -70,23 +63,17 @@ exports.updateBootcamp = async (req, res, next) => {
         });
 
         if (!bootcamp) {
-            return res.status(400).json({
-                success: false
-            });
+            return next(new ErrorResponse(`Bootcamp not found  id of ${req.params.id}`, 404));
         }
         res.status(200).json({
             success: true,
             data: bootcamp
         });
     } catch (err) {
-        res.status(400).json({
-            success: false
-        });
+        next(err);
+
     }
-
-
-
-}
+};
 //@desc delete bootcamps
 //@route Get /api/v1/bootcamps/:id
 //access public
@@ -95,17 +82,13 @@ exports.deleteBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
         if (!bootcamp) {
-            return res.status(400).json({
-                success: false
-            });
+            return next(new ErrorResponse(`Bootcamp not found  id of ${req.params.id}`, 404));
         }
         res.status(200).json({
             success: true,
             data: {}
         });
     } catch (err) {
-        res.status(400).json({
-            success: false
-        });
+        next(err);
     }
 }
